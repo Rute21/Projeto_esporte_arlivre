a03e5f4bfd231ec83b20123d9cfdeee77d41dbae
Ana Rute Da Silva Santos <ana.silvasantos@aluno.senai.br>
2026-08-13 16:54:44 -0300

    CRIAÇÃO DO ATLETA-COMPONENT

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { AtletaComponent } from './component/atleta-component/atleta-component';
+
+import { HomeComponent } from './component/home-component/home-component';
+
+export const routes: Routes = [
+
+    {
+        path:"",
+        redirectTo:"/home",
+        pathMatch:"full"
+        
+    },
+
+    {
+        path:"home",
+        component: HomeComponent,
+    },
+
+    {
+        path:"cadastroAtleta",
+        component: AtletaComponent,
+    }
+];
