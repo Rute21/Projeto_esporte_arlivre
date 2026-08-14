14c1a6eb42b32c35d1f5937628c9988fdc629876
Ana Rute Da Silva Santos <ana.silvasantos@aluno.senai.br>
2026-08-14 14:31:38 -0300

    Alteraçoes e melhorias nas demais pastas

--- src/app/component/atleta-component/atleta-component.ts
+++ src/app/component/atleta-component/atleta-component.ts
@@ -1,9 +1,36 @@
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-atleta-component',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './atleta-component.html',
   styleUrl: './atleta-component.css',
 })
-export class AtletaComponent {}
+export class AtletaComponent {
+  //DECLARANDO ATRIBUTOS
+  nome = ''
+  cpf = 0
+  sexo = ''
+  cep = 0
+  ruaLogradouro = ''
+  bairro = ''
+  cidade = ''
+  uf = ''
+
+//DECLARAÇÃO DE FUNÇÕES
+exibirDados(){
+  console.log(this.nome, this.cpf, this.sexo, this.cep,
+    this.uf, this.ruaLogradouro, this.bairro, this.cidade)
+}
+  limparDados(){
+    this.nome = ''
+    this.cpf = 0
+    this.sexo = ''
+    this.cep = 0
+    this.uf = ''
+    this.ruaLogradouro = ''
+    this.bairro = ''
+    this.cidade =''
+  }
+
+}
