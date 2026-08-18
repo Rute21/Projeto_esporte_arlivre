35eb7e2e11a8a4e74a416351d51dde6672a0cff7
Ana Rute Da Silva Santos <ana.silvasantos@aluno.senai.br>
2026-08-18 16:36:08 -0300

    desenvolvimentos no lista-atleta.ts para melhoria do cadastro

--- src/app/component/lista-atleta/lista-atleta.ts
+++ src/app/component/lista-atleta/lista-atleta.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Atleta } from '../../models/Atleta';
 import { AtletaService } from '../../service/atleta-service';
 import { Router } from '@angular/router';
+import { signal } from '@angular/core'
 
 @Component({
   selector: 'app-lista-atleta',
@@ -9,44 +10,51 @@ import { Router } from '@angular/router';
   templateUrl: './lista-atleta.html',
   styleUrl: './lista-atleta.css',
 })
-export class ListaAtleta implements OnInit {
+export class ListaAtleta {
 
-    listaAtletas: Atleta[] = [];
+    //listaAtletas: Atleta[] = [];
+    listaAtletas = signal<Atleta[]>([]);
   
-    constructor(
-      private listaService: AtletaService,
-      private router: Router
-    ) {}
+    constructor(private listaService: AtletaService,private router: Router ) {}
   
-    ngOnInit(): void {
+    ngOnInit(){
       this.listar();
     }
   
-    listar(): void {
-      this.listaService.listarAtletas().subscribe({
+    listar(){
+      this.listaService.listarAtletas()
+      .subscribe({
         next: (dadosAtletas: Atleta[]) => {
-          this.listaAtletas = [...dadosAtletas].sort((a, b) =>
-            a.nome.localeCompare(b.nome)
-          );
-  
-          console.table(this.listaAtletas);
+          //this.listaAtletas = [...dadosAtletas].sort((a, b) => a.nome.localeCompare(b.nome));
+            this.listaAtletas.set([...dadosAtletas].sort((a,b) =>
+             a.nome.localeCompare(b.nome)))
         },
         error: (msgErro: any) => {
           console.log("Erro ao listar Atletas", msgErro);
         }
-      });
+      })
+
     }
   
-    // Adicione este método para tratar a exclusão
     excluir(id: number) {
-      console.log('Excluir atleta com id:', id);
-      // Adicione aqui a chamada para o seu service (ex: this.listaService.deletar(id)...)
+      if(confirm("Deseja Excluir o Atleta?")){
+        this.listaService.excluirAtleta(id)
+        .subscribe({
+          next: (resposta)=> {
+            console.log("Excluído com Sucesso!!", resposta)
+            this.listar()
+          },
+          error:(msgErro)=>{
+            console.log("Erro ao listar Atletas", msgErro)
+          }
+        })
+      }
     }
   
-    // Adicione este método para carregar o formulário de alteração
+   
     carregaDadosAtletaForm(atleta: Atleta) {
-      console.log('Carregar dados do atleta:', atleta);
-      // Adicione aqui a lógica de navegação ou edição
+      this.router.navigate(['/cadastroAtleta', atleta.id])
+      
     }
   }
   
